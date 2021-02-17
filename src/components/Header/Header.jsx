2dc5ecaa33524c7c0d22f03cc7d457f6280d348a
polyvit <polina98_08@mail.ru>
2021-02-17 12:35:49 +0300

    commit message login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
-        <img src='https://lh3.googleusercontent.com/proxy/iZYjq3Aer6uqJ9-HriY86EQ3svof8PVPARZh53CcH16Ph0vDal1efe0a1TQi-gQcURdL-e-NU-hmlI_TWVJlwSXKU18siVsMf9KESagBgBPF2QykfSgqyVBb-YIRrjnwBxUzDRP6kQ'></img>
+        <img src='https://lh3.googleusercontent.com/proxy/iZYjq3Aer6uqJ9-HriY86EQ3svof8PVPARZh53CcH16Ph0vDal1efe0a1TQi-gQcURdL-e-NU-hmlI_TWVJlwSXKU18siVsMf9KESagBgBPF2QykfSgqyVBb-YIRrjnwBxUzDRP6kQ'/>
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 

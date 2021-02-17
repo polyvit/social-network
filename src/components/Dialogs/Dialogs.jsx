33e7a984438892d97264928684c075400bf05877
polyvit <polina98_08@mail.ru>
2021-02-17 11:36:01 +0300

    Test commit 3

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
+// import {sendMessageCreator, updateNewMessageBodyCreator} from "../../Redux/dialogs-reducer";
+
+const Dialogs = (props) => {
+
+    let state = props.dialogsPage
+
+    let dialogsElements = state.dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id}/>)
+    let messagesElements = state.messages.map( message => <Message message={message.message}/>)
+
+    let onNewMessageChange = (e) => {
+        let body = e.target.value;
+        // props.store.dispatch(updateNewMessageBodyCreator(body))
+        props.updateNewMessageBody(body);
+    }
+
+    let onSendMessageClick = () => {
+        // props.store.dispatch(sendMessageCreator())
+        props.sendMessage()
+    }
+
+    return (
+        <div>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    {dialogsElements}
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div>{messagesElements}</div>
+            </div>
+            <div>
+                <div>
+                    <textarea value={state.newMessageBody} onChange={onNewMessageChange} placeholder='Write your message'></textarea>
+                </div>
+                <div>
+                    <button onClick={onSendMessageClick}>Send</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dialogs

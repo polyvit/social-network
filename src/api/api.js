63b6d1da0f01dc6a8b9ae7a91d09baa728bbed14
polyvit <polina98_08@mail.ru>
2021-02-20 12:18:03 +0300

    new api updatestatus

--- src/api/api.js
+++ src/api/api.js
@@ -33,7 +33,7 @@ export const profileAPI = {
         return instance.get(`status/` + userId)
     },
     updateStatus(status) {
-        return instance.put(`status/`)
+        return instance.put(`status/`, {status})
     },
 }
 

a0d7cf9d9bf206a3911af7ce6feac1aa3191bd71
polyvit <polina98_08@mail.ru>
2021-02-21 17:03:21 +0300

    commit message

--- src/components/Common/FormsControls/FormsControls.js
+++ src/components/Common/FormsControls/FormsControls.js
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./FormsControls.module.css"
 
 export const Textarea = ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error
+
     return (
-        <div className={styles.formControl + " " + styles.error}>
+        <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
             <div>
                 <textarea {...input} {...props} />
             </div>
-            <span>{"some error"}</span>
+            {hasError && <span>{meta.error}</span>}
         </div>
     )
 }

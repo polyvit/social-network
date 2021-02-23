4e80e356990eadd25ebc1e56d48ba3ad4c47285d
polyvit <polina98_08@mail.ru>
2021-02-23 13:44:03 +0300

    Add selectors to mstp

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,6 +9,13 @@ import Users from "./Users";
 import Preloader from "../Common/preloader/preloader";
 import {compose} from "redux";
 import {withAuthRedirect} from "../../HOC/withAuthRedirect";
+import {
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize, getStateUsers,
+    getTotalUsersCount
+} from "../../Redux/users-selectors";
 
 class UsersContainer extends React.Component {
 
@@ -37,14 +44,16 @@ class UsersContainer extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress,
+        users: getStateUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state),
     }
 }
+
+
 
 export default compose(
     withAuthRedirect,

71870313879790c2715aeda4fd8d663c2ba2571d
polyvit <polina98_08@mail.ru>
2021-02-26 19:10:44 +0300

    Add createField

--- src/components/Common/FormsControls/FormsControls.js
+++ src/components/Common/FormsControls/FormsControls.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./FormsControls.module.css"
+import {Field} from "redux-form";
 
 export const Textarea = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error
@@ -26,3 +27,11 @@ export const Input = ({input, meta, ...props}) => {
     )
 }
 
+export const createField = (placeholder, name, validators, component, props={}, text="") => {
+    return (<div>
+        <Field placeholder={placeholder} name={name}
+               validate={validators} component={component}
+               {...props}
+        /> {text}
+    </div>)
+}
